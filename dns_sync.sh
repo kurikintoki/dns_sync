4589e5ea2c9dc8174d0f3a71d4c5092e21f84a57
kurikintoki <karakuriuzumaki@gmail.com>
2015-05-01 10:53:42 +0900

    comment "/etc/named\*" out

--- dns_sync.sh
+++ dns_sync.sh
@@ -3,7 +3,7 @@
 user=root
 targetip=1.1.1.1
 
-/usr/bin/rsync -avz /etc/named* $user@$targetip:/etc
+#/usr/bin/rsync -avz /etc/named* $user@$targetip:/etc
 /usr/bin/rsync -avz /etc/smbind $user@$targetip:/etc
 /usr/bin/rsync -avz /var/named $user@$targetip:/var
 
